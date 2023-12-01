8ee415f2c080a28e9504856ce659573e79204680
Jan Tore Kyrdalen <jan.tore.kyrdalen@noaignite.com>
2023-12-01 14:02:27 +0100

    Day 1

---
+++ day1_trebuchet/day1_trebuchet_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from day1_trebuchet.trebuchet import get_calibration
+
+
+class TestDay1Methods(unittest.TestCase):
+
+    def test_get_calibration_part1(self):
+        # Arrange
+        input = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
+        expected = [12, 38, 15, 77]
+        # Act
+        actual = get_calibration(input)
+        # Assert
+        self.assertEqual(actual, expected)
+        self.assertEqual(sum(actual), 142)
+
+    def test_get_calibration_part2(self):
+        # Arrange
+        input = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+        expected = [29, 83, 13, 24, 42, 14, 76]
+        # Act
+        actual = get_calibration(input)
+        # Assert
+        self.assertEqual(actual, expected)
+        self.assertEqual(sum(actual), 281)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ day1_trebuchet/trebuchet.py
@@ -0,0 +1,46 @@
+
+from enum import Enum
+
+
+class Numbers(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+
+
+def get_calibration(input: list[str]) -> list[int]:
+    calibration = []
+    for line in input:
+        digits_in_line = digits(line)
+        digits_in_line.update(digits_spelled_out(line))
+        number = min(digits_in_line.items(), key=lambda x: x[0])[1]
+        number += max(digits_in_line.items(), key=lambda x: x[0])[1]
+        calibration.append(int(number))
+    return calibration
+
+
+def digits(line: str) -> dict[int, str]:
+    digits = {}
+    for i, char in enumerate(line):
+        if char.isnumeric():
+            digits[i] = char
+    return digits
+
+def digits_spelled_out(line: str) -> dict[int, str]:
+    digits = {}
+    for number in Numbers:
+        if number.name.lower() in line.lower():
+            digits[line.find(number.name.lower())] = str(number.value)
+    return digits
+   
+
+if __name__ == '__main__':
+    with open("input.txt", "r") as input:
+        calibration_values = get_calibration(input.readlines())
+    print(sum(calibration_values))
