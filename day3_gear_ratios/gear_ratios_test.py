4937ff08d55e997f989aa672e94ff96027d67c05
Jan Tore Kyrdalen <jtkyrdalen@gmail.com>
2023-12-04 18:34:01 +0100

    gear ratios

--- day3_gear_ratios/gear_ratios_test.py
+++ day3_gear_ratios/gear_ratios_test.py
@@ -14,7 +14,8 @@ TEST_INPUT = [
                 "......755.",
                 "...$.*....",
                 ".664.598.."]
-TEST_RESULT = 4361
+SUM_PART_NUMBERS = 4361
+SUM_GEAR_RATIOS = 467835
     
 
 class TestDay3(unittest.TestCase):
@@ -35,4 +36,12 @@ class TestDay3(unittest.TestCase):
         # Act
         sum = engine.sum_part_numbers()
         # Assert
-        self.assertEqual(TEST_RESULT, sum)
+        self.assertEqual(SUM_PART_NUMBERS, sum)
+
+    def test_sum_gear_ratios(self):
+        # Arrange
+        engine = Engine(TEST_INPUT)
+        # Act
+        sum = engine.sum_gear_ratios()
+        # Assert
+        self.assertEqual(SUM_GEAR_RATIOS, sum)
